2757e9976493cc46df62c33b73a26f9b9058fb19
MCGSP <stratomeierpleynet@outlook.com>
2025-05-15 09:18:45 +0200

    Update Leih- und Tauschbörse.py

--- Leih- und Tauschbörse.py
+++ Leih- und Tauschbörse.py
@@ -1,13 +1,28 @@
-artikel = []
-wahl = input("Willkommen bei der Leih- und Tauschbörse. Möchtest du einen Artikel hinzufügen, leihen oder tauschen? Dann gib das entsprechende Wort ein: ")
-while wahl != "x":
+artikel = [] # Liste wird erstellt
+print("Willkommen bei der Leih- und Tauschbörse!")
+wahl = input("Möchtest du einen Artikel hinzufügen, leihen oder tauschen? Dann gib das entsprechende Wort ein: ")
+while wahl != "x": # Schleife die sich nur bei der Eingabe "x" vom Benutzer beendet
     if wahl == "hinzufügen":
-        neuerArtikel = input("Was möchtest du hinzufügen?: ")
-        artikel.insert(0,neuerArtikel)
+        neuerArtikel = input("Was möchtest du hinzufügen?: ") # Variable um genau den Artikel den 
+        artikel.insert(0,neuerArtikel)                        # der Benutzer eingibt hinzuzufügen
         
     elif wahl == "leihen":
-        for i in range(0, len(artikel)):
+        print("Diese Artikel sind verfügbar:")
+        for i in range(0, len(artikel)): # Zählschleife um alle Artikel anzuzeigen
             print(artikel[i])
-        artikel("Diese Artikel sind verfügbar. Welchen möchtest du leihen?: ")
+        artikelLeihen = input("Welchen möchtest du leihen?: ")
+        for i in range(0, len(artikel)):
+            if artikel[i] == artikelLeihen: # Jeder Durchgang der Zählschleife prüft einen Artikel in der Liste,
+                artikel.pop(i)              # ob er der vom Benutzer ausgeliehene Artikel ist und entfernt ihn dann
         
+    elif wahl == "tauschen":
+        print("Diese Artikel sind verfügbar:")
+        for i in range(0, len(artikel)): # Zählschleife um alle Artikel anzuzeigen
+            print(artikel[i])
+        eintauschArtikel = input("Welchen Artikel gibst du ab?: ")
+        artikelTausch = input("Und welchen Artikel nimmst du dafür?: ")
+        for i in range(0, len(artikel)):
+            if artikel[i] == eintauschArtikel:
+                artikel[i] = artikelTausch
+    
     wahl = input("Möchtest du noch einen Artikel hinzufügen, leihen oder tauschen? Gib andernfalls x ein um das Programm zu beenden: ")
