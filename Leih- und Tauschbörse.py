983903130c1c832ae627ac07bf4cef19a530a1f9
MCGSP <stratomeierpleynet@outlook.com>
2025-05-19 12:14:02 +0200

    Update Leih- und Tauschbörse.py

--- Leih- und Tauschbörse.py
+++ Leih- und Tauschbörse.py
@@ -1,6 +1,7 @@
 artikel = [] # Liste wird erstellt
 print("Willkommen bei der Leih- und Tauschbörse!")
 wahl = input("Möchtest du einen Artikel hinzufügen, leihen oder tauschen? Dann gib das entsprechende Wort ein: ")
+
 while wahl != "x": # Schleife die sich nur bei der Eingabe "x" vom Benutzer beendet
     if wahl == "hinzufügen":
         neuerArtikel = input("Was möchtest du hinzufügen?: ") # Variable um genau den Artikel den 
@@ -22,7 +23,7 @@ while wahl != "x": # Schleife die sich nur bei der Eingabe "x" vom Benutzer been
         eintauschArtikel = input("Welchen Artikel gibst du ab?: ")
         artikelTausch = input("Und welchen Artikel nimmst du dafür?: ")
         for i in range(0, len(artikel)):
-            if artikel[i] == eintauschArtikel:
-                artikel[i] = artikelTausch
+            if artikel[i] == eintauschArtikel: # Wieder eine prüfende Zählschleife, nur wird
+                artikel[i] = artikelTausch     # hier dann der Artikel durch einen neuen ersetzt
     
     wahl = input("Möchtest du noch einen Artikel hinzufügen, leihen oder tauschen? Gib andernfalls x ein um das Programm zu beenden: ")
